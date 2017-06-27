fff5b477404ad64723a88107b3c3a24e2aeb877f
forgoodsj <877848146@qq.com>
2017-06-28 00:52:41 +0800

    Initial commit

---
+++ blog/models.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models
+from django.contrib.auth.models import User
+
+# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+class Tag(models.Model):
+    name = models.CharField(max_length=100)
+
+class Post(models.Model):
+    title = models.CharField(max_length=70)
+    body = models.TextField() #存储大段文本
+
+    created_time = models.DateTimeField()
+    modified_time = models.DateTimeField()
+
+    #摘要，可以为空
+    excerpt = models.CharField(max_length=200, blank=True)
+
+    category = models.ForeignKey(Category)
+    tags = models.ManyToManyField(Tag, blank=True)
+
+    author = models.ForeignKey(User)
+    # # django.contrib.auth 是 Django 内置的应用，专门用于处理网站用户的注册、登录等流程，User 是 Django 为我们已经写好的用户模型。
